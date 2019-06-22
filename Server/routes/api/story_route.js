8eae2213f0399d185cb09b564ac616970dddd23b
japarker02446 <japarker.02446@gmail.com>
2019-06-21 20:51:40 -0400

    Completed and tested Story route

--- Server/routes/api/story_route.js
+++ Server/routes/api/story_route.js
@@ -18,11 +18,18 @@ router.route('/stories')
 // CREATE a new TracKing Story (Requirement or Issue/Bug).
 // The value of index is set to the count of records in the collection + 1.
 // The initial stage is set to the default value "BACKLOG".
+// REFERENCE: https://stackoverflow.com/questions/29532742/how-to-get-number-of-document-mongoose/29532923
 .post(async function(req, res){
+
+    // Get the count of documents in the Story collection.
+    let storyCount = 0;
+    await Story.countDocuments({}, function(err, count){
+        storyCount = count;
+    })
+
     let newStory = new Story({
         project: req.body.project,
-//      index: db.collection.count() + 1,
-        index: 1,
+        index: storyCount + 1,
         description:req.body.description,
         createdBy:req.body.createdBy,
         assignedTo:req.body.assignedTo,
@@ -32,7 +39,8 @@ router.route('/stories')
         load:req.body.load,
         storyStage:{
             stageName: "BACKLOG",
-            startDate: Date.now()
+            startDate: Date.now(),
+            endDate: null
         }
     });
     await newStory.save((err) => {
@@ -47,26 +55,36 @@ router.route('/stories')
 
 // Delete a story by _id: ADMIN ONLY!!!
 .delete(async function(req, res){
-    await Story.findOneAndDelete({'_id': req.id}, (err) => {
-        if(err){
-            res.status(500).send(`Story delete failed: ${err.message}`);
-        }
-        else{
-            res.status(200).send('Story deleted');
-        }
-    });
+    let deletedStory = await Story.findOneAndDelete({'_id': req.body.id});
+    if(deletedStory){
+        res.status(200).send(`Story delete success: ${deletedStory._id}`);
+    }
+    else {
+        res.status(500).send(`Story delete failed: ${req.body.id}`)
+    }
 }) // NOTE - NO SEMICOLON!!!
 
 // Update a story to change information included in the request.
+// REFERENCE: https://stackoverflow.com/questions/47877333/when-using-findoneandupdate-how-to-leave-fields-as-is-if-no-value-provided-i
 .put(async function(req, res){
-    let story = await Story.findOne({'_id':req.body.id});
-    await story.findOneAndUpdate(
-        {'_id': req.body.story._id},
-        {'index': req.body.index},
-        {'assignedTo': req.body.assignedTo},
-        {'epic': req.body.epic},
-        {'priority': req.body.priority}
-    );
+
+    // What values are being updated in the request body?
+    let params = {};
+    for(let prop in req.body){
+        if(req.body[prop]){
+            params[prop] = req.body[prop];
+        }
+    };
+
+    // Report what was updated.
+    let updatedStory = false;
+    updatedStory = await Story.findOneAndUpdate({'_id': req.body.id}, params);
+    if(updatedStory){
+        res.status(200).send(`Story update success: ${updatedStory._id}`);
+    }
+    else {
+        res.status(500).send(`Story update failed: ${req.body.id}`)
+    }
 });
 // END OF router.route('/stories').
 
@@ -75,7 +93,7 @@ router.route('/stories')
 router.put('/storycomment', async function (req,res){
     let story = await Story.findOne({'_id':req.body.id});
 
-    await story.comments.push({comments: req.body});
+    await story.comments.push(req.body.comments);
     await story.save((err) => {
         if(err){
             res.status(500).send(`Story comment save failed: ${err.message}`);
@@ -95,31 +113,45 @@ router.put('/storycomment', async function (req,res){
         When a story changes stage:
             1. It's current stage is given an end date,
             2. Create the new stage with a start date of now.
-    // REFERENCE: https://stackoverflow.com/questions/10920651/get-the-latest-record-from-mongodb-collection/53474483
+    // REFERENCE: https://stackoverflow.com/questions/26156687/mongoose-find-update-subdocument
 */
 router.put('/stagechange', async function (req,res){
-    let story = await Story.findOne({'_id':req.body.id});
-
-    // Add an end date to the last stage.
-    let lastId = await story.find().limit(1).sort({$natural: -1}).id;
-    await story.storyStage.findOneAndUpdate(
-        {'_id': lastId},
-        {'endDate': Date.now()}
+    
+    // We want to find the storyStage array entry with endDate == NULL.
+    // The position of this entry is passed in $.
+    let today = Date.now();
+    let story = null;
+    await Story.findOneAndUpdate(
+        {"_id": req.body.id, "storyStage.endDate":null},
+        {
+            "$set":{
+                "storyStage.$.endDate": today
+            }
+        },
+        function(err, doc){
+            if(err){
+                res.status(500).send(`Story stage update failed: ${err.message}`);
+            }
+            else{
+                story = doc;
+            }
+        }
     );
-
+    
     // Create the next stage
-    story.storyStage.push({
-        'stageName': req.body.stageName,
-        'startDate': Date.now()
-    })
+    await story.storyStage.push({
+        'stageName':req.body.stageName,
+        'startDate':today,
+        'endDate':null
+    });
     
     // Save the changes
     await story.save((err) => {
         if(err){
-            res.status(500).send(`Story stage save failed: ${err.message}`);
+            res.status(500).send(`Story stage update failed: ${err.message}`);
         }
         else{
-            res.status(200).send('Story stage saved');
+            res.status(200).send(`Story stage update success: ${story._id}`);
         }
     })
 });
