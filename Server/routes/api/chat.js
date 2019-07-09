d5e09b3363a346b2427c75814f882b8264323814
japarker02446 <japarker.02446@gmail.com>
2019-07-09 18:35:05 -0400

    Created a GET Chat route

---
+++ Server/routes/api/chat.js
@@ -0,0 +1,15 @@
+/*  Client facing API for Chat management functions.
+    - Retrieve existing cards (GET)
+    Use async/await to give functions synchronous behavior.
+*/
+const router = require('express').Router();
+const {Chat} = require('../../models/Chat');
+
+// All routes go to ./api/chat/
+router.route('/chat')
+
+// Retrieve ALL Chat messages for a project.
+.get(async function (req,res){
+    let messages = await Chat.find(req.body.project);
+    res.status(200).send({chat: messages});
+});
