fadd919df9e9644d63eec36830c96091c23d8174
Don Charest <doncharest@gmail.com>
2019-07-22 20:15:13 -0400

    Add emojis to ribbon

--- Client/src/containers/auth/Logout.js
+++ Client/src/containers/auth/Logout.js
@@ -13,7 +13,7 @@ export class Logout extends Component {
     return (
       <Fragment>
         <NavLink onClick={this.props.logout} href="#">
-          Logout
+          &#128682;Logout
         </NavLink>
       </Fragment>
     );
